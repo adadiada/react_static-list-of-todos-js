9cae2db642a540467045be2a26f7a0a7eeb627ab
Hanna Yatskiv <hanna.ada7@gmail.com>
2025-12-04 18:23:02 +0300

    add task solution

--- src/components/TodoInfo/TodoInfo.jsx
+++ src/components/TodoInfo/TodoInfo.jsx
@@ -1,4 +1,4 @@
-import { UserInfo } from "../UserInfo/UserInfo";
+import { UserInfo } from '../UserInfo/UserInfo';
 
 // Add the required props
 export const TodoInfo = ({ todo }) => (
@@ -7,6 +7,6 @@ export const TodoInfo = ({ todo }) => (
   >
     <h2 className="TodoInfo__title">{todo.title}</h2>
 
-    <UserInfo user={todo.user} />
+    {todo.user && <UserInfo user={todo.user} />}
   </article>
 );
